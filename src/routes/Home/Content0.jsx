60b13bd3fa58613850e06b28a7001c42c36b39c4
lichuankai <1054191102@qq.com>
2017-10-31 09:34:54 +0800

    第一块

--- src/routes/Home/Content0.jsx
+++ src/routes/Home/Content0.jsx
@@ -11,65 +11,152 @@ class Banner extends React.Component {
   render() {
     const props = { ...this.props };
     delete props.isMode;
-    const childrenData = [
-      {
-        //title: '<img width="100%" src="https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png" />',
-        content: '让数据说话      用心助力创新',
-        button: 'Learn More',
-      },
-      {
-        //title: '<img width="100%" src="https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png" />',
-        content: '移动端应用下载入口',
-        button: 'Learn More',
-      },
-      {
-        //title: '<img width="100%" src="https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png" />',
-        content: '微信订阅号 知识产权随身宝',
-        button: 'Learn More',
-      }
-    ];
-    const childrenToRender = childrenData.map((item, i) => {
-      //const title = item.title;
-      const content = item.content;
-      const button = item.button;
-      return (<Element
-        key={i}
+    const childrenEles = [
+      (<Element key={0}
         prefixCls="banner-user-elem"
       >
         <BgElement
-          className={`bg bg${i}`}
+          className={`bg bg${0}`}
           key="bg"
         />
         <QueueAnim
           type={['bottom', 'top']} delay={200}
           className={`${props.className}-title`}
           key="text"
-          id={`${props.id}-wrapperBlock${i}`}
+          id={`${props.id}-wrapperBlock${0}`}
         >
           <span
             className="logo"
             key="logo"
-            id={`${props.id}-titleBlock${i}`}
-            //dangerouslySetInnerHTML={{
-            //  __html: title,
-            //}}
+            id={`${props.id}-titleBlock${0}`}
+          //dangerouslySetInnerHTML={{
+          //  __html: title,
+          //}}
           />
-          <p
-            key="content"
-            id={`${props.id}-contentBlock${i}`}
+          <h1
+            key="contentOne"
+            id={`${props.id}-contentBlock${0}`}
           >
-            {content}
-          </p>
+            让数据说话      用心助力创新
+          </h1>
+          <h2
+            key="contentTwo"
+            id={`${props.id}-contentBlock${0}`}
+          >
+            100万+企业的共同选择
+          </h2>
           <Button
             type="ghost"
             key="button"
-            id={`${props.id}-buttonBlock${i}`}
+            className="retrieval"
+            id={`${props.id}-buttonBlock${0}`}
           >
-            {button}
+            专利检索
           </Button>
-        </QueueAnim>
-      </Element>);
-    });
+        </QueueAnim></Element>),
+      (<Element key={1}
+        prefixCls="banner-user-elem"
+      >
+        <BgElement
+          className={`bg bg${1}`}
+          key="bg"
+        />
+        <QueueAnim
+          type={['bottom', 'top']} delay={200}
+          className={`${props.className}-title`}
+          key="text"
+          id={`${props.id}-wrapperBlock${1}`}
+        >
+          <span
+            className="logo"
+            key="logo"
+            id={`${props.id}-titleBlock${1}`}
+          //dangerouslySetInnerHTML={{
+          //  __html: title,
+          //}}
+          />
+          <div className="content">
+            <h1
+              key="contentOne"
+              id={`${props.id}-contentBlock${1}`}
+            >
+              知识产权一点通
+          </h1>
+            <h2
+              key="contentTwo"
+              id={`${props.id}-contentBlock${1}`}
+            >
+              随身携带的全球专利文献数据
+          </h2>
+            <div className="clearfax">
+              <div className="div1"><img src="/downQRcode.png" /></div>
+              <div className="div2">
+                  <a className="apple" href="https://itunes.apple.com/cn/app/zhi-shi-chan-quan-yi-dian-tong/id892264333?mt=8 " target="_blank"><i className="icon-index-apple"></i>APP Store</a>
+                  <a className="android" href="http://www.sdips.gov.cn/attachment/APP-Android.apk " target="_blank"><i></i>Android</a>
+              </div>
+            </div>
+          </div>
+
+        </QueueAnim></Element>),
+      (<Element key={2}
+        prefixCls="banner-user-elem"
+      >
+        <BgElement
+          className={`bg bg${2}`}
+          key="bg"
+        />
+        <QueueAnim
+          type={['bottom', 'top']} delay={200}
+          className={`${props.className}-title`}
+          key="text"
+          id={`${props.id}-wrapperBlock${2}`}
+        >
+          <span
+            className="logo"
+            key="logo"
+            id={`${props.id}-titleBlock${2}`}
+          //dangerouslySetInnerHTML={{
+          //  __html: title,
+          //}}
+          />
+          <div className="content03">
+            <h1
+              className="title1"
+              key="contentOne"
+              id={`${props.id}-contentBlock${2}`}
+            >
+            微信订阅号 知识产权随身宝
+          </h1>
+            <h2
+            className="title2"
+              key="contentTwo"
+              id={`${props.id}-contentBlock${2}`}
+            >
+            知识产权从未如此聚焦
+          </h2>     
+              <div className="weixin"><img src="/downQRcode.jpg" /></div>
+          </div>
+
+        </QueueAnim></Element>),
+    ];
+    // const childrenData = [
+    //   {
+    //     //title: '<img width="100%" src="https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png" />',
+    //     contentOne: '让数据说话      用心助力创新',
+    //     contentTwo: '100万+企业的共同选择',
+    //     button: '专利检索',
+    //   },
+    //   {
+    //     //title: '<img width="100%" src="https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png" />',
+    //     content: '移动端应用下载入口',
+    //     button: 'Learn More',
+    //   },
+    //   {
+    //     //title: '<img width="100%" src="https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png" />',
+    //     content: '微信订阅号 知识产权随身宝',
+    //     button: 'Learn More',
+    //   }
+    // ];
     return (
       <OverPack
         {...props}
@@ -84,7 +171,7 @@ class Banner extends React.Component {
             <BannerAnim
               key="banner"
             >
-              {childrenToRender}
+              {childrenEles}
             </BannerAnim>
           </div>
         </TweenOneGroup>
