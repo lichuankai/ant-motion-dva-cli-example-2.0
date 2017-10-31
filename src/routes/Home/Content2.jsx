3194f14ed0a7c4b999fc22e54dc8553f5aa05d83
liduopie <liduopie@163.com>
2017-10-31 14:21:59 +0800

    添加维权援助

--- src/routes/Home/Content2.jsx
+++ src/routes/Home/Content2.jsx
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-import { Button, Icon } from 'antd';
+import { Button } from 'antd';
 import QueueAnim from 'rc-queue-anim';
 import TweenOne, { TweenOneGroup } from 'rc-tween-one';
 import BannerAnim, { Element } from 'rc-banner-anim';
@@ -10,7 +10,94 @@ const BgElement = Element.BgElement;
 class Banner extends React.Component {
   render() {
     const props = { ...this.props };
+    const isMode = props.isMode;
     delete props.isMode;
+    const animType = {
+      queue: isMode ? 'bottom' : 'left',
+      one: isMode ? {
+        y: '+=30', opacity: 0, type: 'from', ease: 'easeOutQuad',
+      }
+        : {
+          x: '+=30', opacity: 0, type: 'from', ease: 'easeOutQuad',
+        },
+    };
+
+    const childrenElements = [
+      <Element key="0" prefixCls="banner-user-elem">
+        <QueueAnim
+          type={['bottom', 'top']} delay={200}
+          className={`${props.className}-title`}
+          key="text"
+          id={`${props.id}-wrapperBlock0`}
+        >
+          <TweenOne
+            key="img"
+            animation={animType.one}
+            className={`${props.className}-img`}
+            id={`${props.id}-imgWrapper`}
+            resetStyleBool
+          >
+            <span id={`${props.id}-img`}>
+              <img src="/one.png" width="100%" alt="a" />
+            </span>
+          </TweenOne>
+          <QueueAnim
+            type={animType.queue}
+            className={`${props.className}-text`}
+            key="text"
+            leaveReverse
+            ease={['easeOutCubic', 'easeInCubic']}
+            id={`${props.id}-textWrapper`}
+          >
+            <h1 key="h1" id={`${props.id}-title`}>
+              知识产权维权援助
+            </h1>
+            <p key="p" id={`${props.id}-content`}>
+              维权援助与举报电话：<br />
+              <br />
+              <img src="/12330.png" width="80%" alt="a" />
+            </p>
+          </QueueAnim>
+        </QueueAnim>
+      </Element>,
+      <Element key="1" prefixCls="banner-user-elem">
+        <QueueAnim
+          type={['bottom', 'top']} delay={200}
+          className={`${props.className}-title block1`}
+          key="text"
+          id={`${props.id}-wrapperBlock1`}
+        >
+          <TweenOne
+            key="img"
+            animation={animType.one}
+            className={`${props.className}-img`}
+            id={`${props.id}-imgWrapper`}
+            resetStyleBool
+          >
+            <span id={`${props.id}-img`}>
+              <img src="/yidian.png" width="50%" />
+            </span>
+          </TweenOne>
+          <QueueAnim
+            type={animType.queue}
+            className={`${props.className}-text`}
+            key="text"
+            leaveReverse
+            ease={['easeOutCubic', 'easeInCubic']}
+            id={`${props.id}-textWrapper`}
+          >
+            <h1 key="h1" id={`${props.id}-title`}>
+              采用移动端的维权取证
+            </h1>
+            <p key="p" id={`${props.id}-content`}>
+              文字、录音、录像、拍照取证全覆盖
+              <br />
+              <img src="/downQRcode.png" />
+            </p>
+          </QueueAnim>
+        </QueueAnim>
+      </Element>,
+    ];
     const childrenData = [
       {
         title: '<img width="100%" src="https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png" />',
@@ -84,18 +171,10 @@ class Banner extends React.Component {
             <BannerAnim
               key="banner"
             >
-              {childrenToRender}
+              {childrenElements}
             </BannerAnim>
           </div>
         </TweenOneGroup>
-        <TweenOne
-          animation={{ y: '-=20', yoyo: true, repeat: -1, duration: 1000 }}
-          className={`${props.className}-icon`}
-          style={{ bottom: 40 }}
-          key="icon"
-        >
-          <Icon type="down" />
-        </TweenOne>
       </OverPack>
     );
   }
@@ -106,7 +185,7 @@ Banner.propTypes = {
 };
 
 Banner.defaultProps = {
-  className: 'banner1',
+  className: 'banner2',
 };
 
 export default Banner;
